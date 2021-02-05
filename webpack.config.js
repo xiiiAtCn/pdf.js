0e835b0879f87fe1a23263f1099c4b7817abcb5c
gao.qiang <gao.qiang_202373@immomo.com>
2021-02-05 14:02:47 +0800

    名称调整

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle[hash:10].js",
+    filename: "name.[hash:10].js",
   },
   plugins: [
     new CleanWebpackPlugin(),
